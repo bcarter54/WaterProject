f540f2b8d74ff84b313311dd7518940d7de25dad
Bryce Carter <carterbryce54@gmail.com>
2025-04-02 16:34:33 -0600

    Fix

--- frontend/src/Fingerprint.tsx
+++ frontend/src/Fingerprint.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { getFingerprint } from '@thumbmarkjs/thumbmarkjs';
 
 function Fingerprint() {
